41d6688cfac9706977a969dd430e948d1ab176d7
pavanrao6232 <125258498+pavanrao6232@users.noreply.github.com>
2023-02-13 17:01:25 +0530

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azureml-examples"
+GROUP="rg-MLOps-kcl-eastus-001"
 LOCATION="eastus"
-WORKSPACE="main"
+WORKSPACE="azure-mlops"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
